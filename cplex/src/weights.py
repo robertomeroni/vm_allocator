7f375139203afcd854dd418f455aff1fb6d75e0f
robertomeroni <inbox1rob@gmail.com>
2024-09-12 14:25:40 +0200

    new energy costs

--- cplex/src/weights.py
+++ cplex/src/weights.py
@@ -1,14 +1,13 @@
 from config import TIME_STEP
 
 main_time_step = TIME_STEP  # in seconds
-time_window = 10 * TIME_STEP  # in seconds
+time_window = 20 * TIME_STEP  # in seconds
 
 # Conversion factors
 hr_to_s = 3.6 * 10**3  # hours to seconds
 kWh_to_J = 3.6 * 10**6  # kilowatt-hours to joules
+bytes_to_GB = 10**(-9)  # bytes to gigabytes
 
-# Weights
-network_bandwidth = 100.0  # GB/s
 
 pue = 1.5  # Power Usage Effectiveness
 
@@ -23,16 +22,19 @@ energy = {
 }
 
 migration = {
-    'time': {  # in seconds, offset + coefficient * memory_size
-        'offset': 0.0,  
-        'coefficient': 1 / network_bandwidth  
+    'time': {  # in seconds
+        'memory_dirty_rate': 0.1,  # GB of memory that gets dirty per second during live migration
+        'network_bandwidth': 10.0,  # GB/s
+        'resume_vm_on_target': 20.0 / 10**3,  # in seconds
     },
-    'energy': {  # in Joules, offset + coefficient * memory_size
-        'offset': 20165,  
-        'coefficient': 512.0
-    },
-    'penalty': 0.1  
+    'energy': {  
+        'cpu_overhead': {
+            'source': 0.015,
+            'target': 0.017,
+        },
+        'concurrent': 0.016
+    }
 }
 
 w_load_cpu = 0.8 # How much the load of the CPU affects the energy consumption (compared to memory)
-
+expected_runtime_factor = 0.9  # What is the expected real completion time of a task compared to the declared run time: run time / real completion time (allocation time + run time + eventual migration time)
